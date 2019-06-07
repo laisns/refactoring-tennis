f7727b0d4cd7c01fa5dd5583aaeeb76e1305e898
Laís Nascimento <ls_santos@id.uff.br>
2019-06-07 18:42:20 -0300

    added spec test, added file to analyze flog score

--- tennis_spec.rb
+++ tennis_spec.rb
@@ -1,33 +1,34 @@
 require File.join(File.dirname(__FILE__ ), 'tennis')
+require 'pry'
 
 describe TennisGame1 do
-  def new_game(gameClass, p1_name, p2_name, p1_points, p2_points)
-  # it ' increase p1 points' do
-  #   game = TennisGame1.new('abc', 'def')
-  #   game.won_point('abc')
-  #   expect(game).to eq 'x'
-  # end
-    game = gameClass.new(p1_name, p2_name)
-    (0..[p1_points, p2_points].max).each do |p|
-      if p < p1_points
-        game.won_point(p1_name)
-      end
-      if p < p2_points
-        game.won_point(p2_name)
-      end
-    end
-    game
-    # TennisGame1.won_point(game.player1Name)
+  it 'creates new players' do
+    game = TennisGame1.new('player1', 'player2')
+    expect(game.score).to eq('Love-All')
   end
 
-  describe 'won#point' do
-    it 'increases p1 points' do
-      game = new_game(TennisGame1, 'abc', 'def', 1, 2)
-      expect(game).to have_attributes(player1Name:'abc', player2Name: 'def', p1_points: 1, p2_points: 3)
-
-
+  it 'informs when a player wins' do
+    game = TennisGame1.new('player1', 'player2')
+    4.times do
+      game.won_point('player1')
     end
+    expect(game.instance_variable_get(:@p1points)).to eq(4)
+    expect(game.score). to eq('Win for player1')
+  end
+end
 
+describe TennisGame2 do
+  it 'creates two new players' do
+    game = TennisGame2.new('player1', 'player2')
+    expect(game.score).to eq('Love-All')
+  end
 
+  it 'informs when a player wins' do
+    game = TennisGame2.new('player1', 'player2')
+    4.times do
+      game.won_point('player2')
+    end
+    expect(game.instance_variable_get(:@p2points)).to eq(4)
+    expect(game.score). to eq('Win for player2')
   end
 end
