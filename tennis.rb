72d86fc7c88ab62c2a0e84e71021e0f0e9c4cedd
Laís Nascimento <ls_santos@id.uff.br>
2019-06-11 16:06:58 -0300

    refactor#2: removed score.rb, new changes in main file, added flog for new refactor

--- tennis.rb
+++ tennis.rb
@@ -1,6 +1,5 @@
-require_relative 'score'
 class TennisGame
-  include Score
+
   attr_reader :p1points, :p2points
 
   def initialize(player1Name, player2Name)
@@ -14,29 +13,47 @@ class TennisGame
     playerName == "player1" ? @p1points += 1 : @p2points += 1
   end
 
-  def score
-    if @p1points < 4 and @p2points < 4 and @p1points + @p2points < 6
-      p = ["Love", "Fifteen", "Thirty", "Forty"]
-      s = p[@p1points]
-      @p1points == @p2points ? s + "-All" : s + "-" + p[@p2points]
-    else
-      if @p1points == @p2points
-        "Deuce"
-      else
-        s = @p1points > @p2points ? @player1Name : @player2Name
-        (@p1points - @p2points) * (@p1points - @p2points) == 1 ? "Advantage " + s : "Win for " + s
-      end
-    end
+  def score(p1, p2)
+
+    score = ['Love', 'Fifteen', 'Thirty', 'Forty']
+    puts "#{score[p1]} - All" if all(p1, p2)
+    puts 'Deuce' if deuce(p1, p2)
+    puts p1 > p2 ? 'win for player 1' : 'win for player 2' if game_won(p1, p2)
+    puts p1 > p2 ? 'advantage player 1' : 'advantage player 2' if advantage(p1, p2)
+    puts "#{score[p1]} - #{score[p2]}" if distinct(p1, p2)
+
   end
+
+  private
+
+  def distinct(x, y)
+    x != y and x < 4 and y < 4
+  end
+
+  def game_won(x, y)
+    (x >= 4 || y >=4) and (x - y).abs > 1
+  end
+
+  def deuce(x, y)
+    x == y and x >= 3
+  end
+
+  def all(x, y)
+    x == y and x < 3
+  end
+
+  def advantage(x, y)
+    x >= 3 and y >= 3 and (x-y).abs == 1
+  end
+
 end
+
 
 
 ###################################################
-game = TennisGame.new('player1', 'player2')
-4.times do
-  game.won_point('player2')
-end
-# puts game.score
+# game = TennisGame.new('player1', 'player2')
+# 3.times do
+#   game.won_point('player2')
+# end
+# puts game.score(game.p1points, game.p2points)
 ############# returns  'Love-Forty' ###############
-
-game.check(game.p1points, game.p2points)
